b1684aba7ed10faebc9b44892f3cebb850a58a95
Sharoar Hossain <maffin.killer007@gmail.com>
2025-08-11 13:48:08 +0500

    feat: add router and new page Listed Books

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import ListedBooks from './pages/ListedBooks.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/listed-books',
+    element: <ListedBooks />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
