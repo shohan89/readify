e11682b1e02fe8d6143e163d60e2858f6dafd712
Sharoar Hossain <maffin.killer007@gmail.com>
2025-08-12 12:04:25 +0500

    feat: add section title

---
+++ src/components/SectionTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SectionTitle = ({ title }) => {
+    return (
+        <div className='mt-24 mb-9 text-center'>
+            <h3 className='text-4xl font-bold'>{title}</h3>
+        </div>
+    );
+};
+
+export default SectionTitle;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import SectionTitle from "../components/SectionTitle";
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <SectionTitle title={'Our Books'} />
         </>
     );
 };
