cb1c822044529e367a667608414d0ae910d87932
Sharoar Hossain <maffin.killer007@gmail.com>
2025-08-12 11:23:53 +0500

    feat: add hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Hero from '../components/Hero';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home Page</h2>
-        </div>
+        <>
+            <Hero />
+        </>
     );
 };
 
