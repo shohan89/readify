c0f67764ebf144d64cae4a1b65f84a7535ad1ac9
shoh4n <maffin.killer007@gmail.com>
2025-08-16 17:56:05 +0600

    feat: make tab clickable

--- src/pages/ListedBooks.jsx
+++ src/pages/ListedBooks.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { Link } from "react-router";
 
 const ListedBooks = () => {
+    const [tabIndex, setTabIndex] = useState(0);
     return (
         <>
             <div className=' max-w-7xl mx-auto text-center bg-gray-100 p-8 rounded-2xl mt-8 text-3xl font-bold'>
@@ -8,13 +10,19 @@ const ListedBooks = () => {
             </div>
             {/* tabs */}
             <div className="max-w-7xl mx-auto flex items-start overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap mt-8">
-                <Link rel="noopener noreferrer" href="#" className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b border-gray-400">
+                <Link 
+                to=''
+                onClick={()=> setTabIndex(0)} 
+                className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 0 ? 'border border-b-0' : 'border-b'} rounded-t-lg border-gray-400`}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                         <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
                     </svg>
                     <span>Read Books</span>
                 </Link>
-                <Link rel="noopener noreferrer" href="#" className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border border-b-0 rounded-t-lg border-gray-400">
+                <Link 
+                to='' 
+                onClick={()=>setTabIndex(1)}
+                className={`flex items-center flex-shrink-0 px-5 py-3 space-x-2 ${tabIndex === 1 ? 'border border-b-0' : 'border-b'} rounded-t-lg border-gray-400`}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
                         <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                         <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
