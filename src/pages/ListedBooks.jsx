87cb484283c2d6b0188d3f771f27dc124bfee3e8
shoh4n <maffin.killer007@gmail.com>
2025-08-16 17:46:52 +0600

    feat: add tabs

--- src/pages/ListedBooks.jsx
+++ src/pages/ListedBooks.jsx
@@ -1,9 +1,26 @@
+import { Link } from "react-router";
 
 const ListedBooks = () => {
     return (
         <>
             <div className=' max-w-7xl mx-auto text-center bg-gray-100 p-8 rounded-2xl mt-8 text-3xl font-bold'>
                 <h2>Listed books</h2>
+            </div>
+            {/* tabs */}
+            <div className="max-w-7xl mx-auto flex items-start overflow-x-auto overflow-y-hidden sm:justify-start flex-nowrap mt-8">
+                <Link rel="noopener noreferrer" href="#" className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b border-gray-400">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
+                        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+                    </svg>
+                    <span>Read Books</span>
+                </Link>
+                <Link rel="noopener noreferrer" href="#" className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border border-b-0 rounded-t-lg border-gray-400">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-4 h-4">
+                        <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+                        <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+                    </svg>
+                    <span>Wishlist Books</span>
+                </Link>
             </div>
         
         </>
